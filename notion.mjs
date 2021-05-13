67e5d9c52a44e18b89a5aa5e206eb535e166e0d2
Kory Smith <kory.smith@kroger.com>
2021-05-13 17:27:16 -0400

    Export more stuff

--- notion.mjs
+++ notion.mjs
@@ -6,7 +6,7 @@ const httpsAgentConfig = new HttpsProxyAgent({
   proxy: `http://KS61347:${process.env.KORY_PASSWORD}@cdcproxy.kroger.com:3128`,
 });
 
-const WROJECTS_DATABASE_ID="a5994ff152d04827a47d253941e69e20"
+export const WROJECTS_DATABASE_ID="a5994ff152d04827a47d253941e69e20"
 
 const notion = new Client({
   auth: process.env.NOTION_TOKEN
@@ -24,3 +24,11 @@ export const wrojectTitles = wrojects.map((wroject) => {
  if (!wroject.properties.Wroject.title[0]) {
    return 
  } return wroject.properties.Wroject.title[0].text.content})
+
+
+ export const saneInProgressWrojects = inProgressWrojects.map(wroject => {
+   return {
+     pageId: wroject.id,
+     name: wroject.properties.Wroject.title[0].text.content
+   }
+ })
