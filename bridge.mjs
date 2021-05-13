5ce4dcd1cd2463686dc17d0d92870b35fc2d6998
Kory Smith <kory.smith@kroger.com>
2021-05-13 17:27:24 -0400

    Continue work on bridgie

--- bridge.mjs
+++ bridge.mjs
@@ -1,5 +1,6 @@
 import { workSubProjects } from "./todoist.mjs";
-import { inProgressWrojects, wrojectTitles } from "./notion.mjs";
+import { Client } from '@notionhq/client';
+import { inProgressWrojects, wrojectTitles, saneInProgressWrojects, WROJECTS_DATABASE_ID } from "./notion.mjs";
 
 const projectsToExclude = ["One-offs", "Wickler", "Someday/Maybe"];
 
@@ -11,18 +12,30 @@ const foo = filteredWorkSubProjects.map((todoistProject) => {
   if (wrojectTitles.includes(todoistProject.name)) {
     return {
       name: todoistProject.name,
-      includedInNotion: true,
       todoistUrl: todoistProject.url,
+      includedInNotion: true,
     };
   } else
     return {
       name: todoistProject.name,
-      includedInNotion: false,
       todoistUrl: todoistProject.url,
+      includedInNotion: false,
     };
 });
 
-console.log(foo);
+const thingsToAddToNotion = foo.filter(thing => {
+  return !thing.includedInNotion
+})
+
+const notion = new Client({
+  auth: process.env.NOTION_TOKEN
+})
+
+
+
+async function bridge() {
+  // Waiting for stuff
+}
 
 /* 
 What are the cases?
