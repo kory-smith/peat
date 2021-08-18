c4aa1b698609575e64530e99ef82a8f6902b5a49
Kory Smith <kory.smith@kroger.com>
2021-08-18 11:00:43 -0400

    Type-ify notion requests

--- src/notion.ts
+++ src/notion.ts
@@ -1,5 +1,6 @@
 import { Client } from "@notionhq/client";
-import _ from "lodash";
+import { DatabaseParent, RichTextText, SelectPropertyValue, TitlePropertyValue } from "@notionhq/client/build/src/api-types";
+import { keyBy } from "lodash";
 
 export const WROJECTS_DATABASE_ID = "a5994ff1-52d0-4827-a47d-253941e69e20";
 export const PROJECTS_DATABASE_ID = "27498ca8-235f-4e49-a0f6-ab10b9d40063";
@@ -8,7 +9,7 @@ const notion = new Client({
   auth: process.env.NOTION_TOKEN,
 });
 
-async function getDatabaseFromId(id) {
+async function getDatabaseFromId(id: string) {
   const databaseResponse = await notion.databases.query({
     database_id: id,
   });
@@ -16,16 +17,22 @@ async function getDatabaseFromId(id) {
   const database = databaseResponse.results;
 
   const massagedDatabase = database.map((page) => {
+    // Title properties
+    const nameProperty = page.properties.Name as TitlePropertyValue
+    const titleObject = nameProperty.title[0] as RichTextText
+    const title = titleObject.text.content
+    // Parent properties
+    const datbaseParent = page.parent as DatabaseParent
     return {
-      name: page.properties.Name.title[0].text.content,
+      name: title,
       id: page.id,
-      status: page.properties.Status.select.name,
-      work: page.parent.database_id === WROJECTS_DATABASE_ID,
-      personal: page.parent.database_id === PROJECTS_DATABASE_ID,
+      status: ( page.properties.Status as SelectPropertyValue ).select.name,
+      work: datbaseParent.database_id === WROJECTS_DATABASE_ID,
+      personal: datbaseParent.database_id === PROJECTS_DATABASE_ID,
     };
   });
 
-  const keyedDatabase = _.keyBy(massagedDatabase, (page) => page.name);
+  const keyedDatabase = keyBy(massagedDatabase, (page) => page.name);
   return keyedDatabase;
 }
 
