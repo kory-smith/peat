f152ea59d71901d092632b6e75789d16f738cb33
Kory Smith <kory.smith@kroger.com>
2021-08-24 16:38:17 -0400

    Rewrite Todoist logic in TypeScript

--- src/todoist.ts
+++ src/todoist.ts
@@ -1,50 +1,28 @@
-import got from "got";
-import { HttpsProxyAgent } from "hpagent";
-import _ from "lodash";
+import TodoistApiREST, { TodoistProject } from "@kory-smith/todoist-api-ts"
+import { keyBy } from "lodash";
 
-// For use only if i'm at work
-const httpsAgentConfig = new HttpsProxyAgent({
-  proxy: `http://KS61347:${process.env.KORY_PASSWORD}@cdcproxy.kroger.com:3128`,
-});
 
-// In case I need to use the sync endpoint
-// const SYNC_ENDPOINT = "https://api.todoist.com/sync/v8/sync"
-// const projectsResponse = await got.post(SYNC_ENDPOINT, {
-//   headers: {
-//     Authorization: `Bearer ${process.env.TODOIST_TOKEN}`
-//   },
-//   responseType: "json",
-//   json: {
-//     resource_types: ["projects"]
-//   },
-//   resolveBodyOnly: true,
-//   agent: { https: httpsAgentConfig },
-// })
-// const projects = projectsResponse.projects
+const todoist = new TodoistApiREST(process.env.TODOIST_TOKEN!);
 
-// But it looks like I can get away with the rest api
-const projectsResponse = await got.get(
-  "https://api.todoist.com/rest/v1/projects",
-  {
-    responseType: "json",
-    // agent: { https: httpsAgentConfig },
-    headers: {
-      Authorization: `Bearer ${process.env.TODOIST_TOKEN}`,
-    },
-  }
-);
-const projects = projectsResponse.body;
+async function getAllProjects() {
+  return await todoist.getAllProjects();
+}
 
-const WORK_PROJECT_ID = projects.find((project) => project.name === "Work").id;
-const PERSONAL_PROJECT_ID = projects.find(
-  (project) => project.name === "Personal"
-).id;
+function getProjectIdFromName(projectName: string, projects: TodoistProject[]) {
+  const project = projects.find((project) => project.name === projectName);
+  return project?.id;
+}
+
+const projects: TodoistProject[] = await getAllProjects();
+
+const WORK_PROJECT_ID = getProjectIdFromName("Work", projects);
+const PERSONAL_PROJECT_ID = getProjectIdFromName("Personal", projects);
 
 const projectsWithHelperData = projects.map((project) => {
   return {
     ...project,
-    work: project.parent === WORK_PROJECT_ID,
-    personal: project.parent === PERSONAL_PROJECT_ID,
+    work: project.parent_id === WORK_PROJECT_ID,
+    personal: project.parent_id === PERSONAL_PROJECT_ID,
   };
 });
 
@@ -52,8 +30,7 @@ const onlyRelevantProjects = projectsWithHelperData.filter(
   (project) => project.work || project.personal
 );
 
-export const allTodoistProjectsKeyed = _.keyBy(
+export const allTodoistProjectsKeyed = keyBy(
   onlyRelevantProjects,
   (project) => project.name
 );
-
