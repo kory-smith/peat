45d91e77893bd05f42cf213e7152c8a70cdddfb9
Kory Smith <kor54e@gmail.com>
2021-12-08 15:11:09 -0500

    add todo

--- src/todoist.ts
+++ src/todoist.ts
@@ -25,6 +25,7 @@ export const getAllTodoistProjectsKeyed = async () => {
     };
   });
 
+  // TODO: RENAME ME
   const onlyRelevantProjects = projectsWithHelperData.filter(
     (project) => project.work || project.personal
   );
