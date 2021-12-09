3029448efbb5fc224adac818bef31ceacbfd4d24
Kory Smith <kory.smith@kroger.com>
2021-12-09 16:10:53 -0500

    implement directiveExecutor

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import {
   PROJECTS_DATABASE_ID,
 } from "./notion";
 import got from "got";
-import { generateDirectives, getProjectTitlesFromProjects } from "./utils";
+import { executeDirectives, generateDirectives, getProjectTitlesFromProjects } from "./utils";
 
 const doWork = async () => {
   const projectsToExclude = [
@@ -23,71 +23,20 @@ const doWork = async () => {
 
   const allNotionProjectsKeyed = await getAllNotionProjectsKeyed();
 
-  const allProjectTitles = getProjectTitlesFromProjects(allTodoistProjectsKeyed, allNotionProjectsKeyed, projectsToExclude)
+  const allProjectTitles = getProjectTitlesFromProjects(
+    allTodoistProjectsKeyed,
+    allNotionProjectsKeyed,
+    projectsToExclude
+  );
 
-  // This does the updating
-  for (let project in masterObject) {
-    const { notion, todoist } = masterObject[project];
-    // There's not a page and we should create one
-    if (
-      !("color" in todoist) &&
-      todoist.status === "In-progress" &&
-      !notion.status
-    ) {
-      const databaseIdToUse = todoist.work
-        ? WROJECTS_DATABASE_ID
-        : PROJECTS_DATABASE_ID;
-      const createNotionPageResponse: any = await createNotionChildPage(
-        databaseIdToUse,
-        todoist.name,
-        "In-progress"
-      );
-      // Might as well make the update while we're here...
-      const _ = await got
-        .post("https://api.todoist.com/rest/v1/tasks", {
-          json: {
-            content: `* [Link to Notion project](${createNotionPageResponse.url})`,
-            project_id: Number(todoist.id),
-          },
-          headers: {
-            Authorization: `Bearer ${process.env.TODOIST_TOKEN}`,
-          },
-        })
-        .catch((e) => console.log(e));
-    } else if (
-      todoist.status === "Completed" &&
-      notion.status !== "Completed"
-    ) {
-      notionClient.pages.update({
-        archived: false,
-        page_id: notion.id,
-        properties: {
-          Status: {
-            type: "select",
-            select: {
-              name: "Completed",
-            },
-          },
-        },
-      });
-    } else if (
-      todoist.status === "In-progress" &&
-      notion.status !== "In-progress"
-    ) {
-      notionClient.pages.update({
-        archived: false,
-        page_id: notion.id,
-        properties: {
-          Status: {
-            type: "select",
-            select: {
-              name: "In-progress",
-            },
-          },
-        },
-      });
-    }
-  }
+  const projectsWithDirectives = generateDirectives(
+    allProjectTitles,
+    allTodoistProjectsKeyed,
+    allNotionProjectsKeyed
+  );
+
+ await executeDirectives(projectsWithDirectives)
+
 };
 
 doWork();
